16a6edc6d69a77c665b571e7ef6da3426e7514a9
chaemd <new7932@naver.com>
2025-06-22 08:49:35 +0900

    튜토리얼 수정

--- lib/screens/Tutorial/tutorial2_2page.dart
+++ lib/screens/Tutorial/tutorial2_2page.dart
@@ -1,16 +1,17 @@
-import 'package:domino/screens/Tutorial/tutorial7_page.dart';
+import 'package:domino/screens/Tutorial/tutorial2_3page.dart';
+import 'package:domino/screens/Tutorial/tutorial6_page.dart';
 import 'package:domino/style/style_tutorial.dart';
 import 'package:flutter/material.dart';
 import 'package:domino/style/styles.dart';
 
-class Tutorial5 extends StatefulWidget {
-  const Tutorial5({super.key});
+class Tutorial2_2 extends StatefulWidget {
+  const Tutorial2_2({super.key});
 
   @override
-  State<Tutorial5> createState() => Tutorial5State();
+  State<Tutorial2_2> createState() => Tutorial2_2State();
 }
 
-class Tutorial5State extends State<Tutorial5> {
+class Tutorial2_2State extends State<Tutorial2_2> {
   int selectedIndex = 100;
 
   @override
@@ -25,27 +26,20 @@ class Tutorial5State extends State<Tutorial5> {
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               //프로그레스 바
-              ProgressBar(4, 4).progressBar(),
+              ProgressBar(1, 4).progressBar(),
               SizedBox(height: 10),
 
               //프로그레스 타이틀
-              ProgressTitle('TO-DO 만들기').progressTitle(),
+              ProgressTitle('제1목표 만들기').progressTitle(),
               SizedBox(height: 17),
 
               //질문
-              TutorialQuestion(
-                      "", '동아리 들어가기', "를 위해", '실천으로 옮길 수 있는 TO-DO는?', 'blue')
+              TutorialQuestion("", '새내기', "로 대학에 입학하는", '도민호를 위한 제1목표는?', 'red')
                   .tutorialQuestion(),
               SizedBox(height: 5),
-
-              // 선택지
               ImageOption(
-                imageUrls: [
-                  'assets/img/option3.png',
-                  'assets/img/option4.png',
-                  'assets/img/option5.png'
-                ],
-                color: 'blue',
+                imageUrls: ['assets/img/option1.png', 'assets/img/option2.png'],
+                color: 'red',
                 onItemSelected: (index) {
                   setState(() {
                     selectedIndex = index;
@@ -63,9 +57,9 @@ class Tutorial5State extends State<Tutorial5> {
             if (selectedIndex == 0) {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => const Tutorial7()),
+                MaterialPageRoute(builder: (context) => const Tutorial2_3()),
               );
-            } else if (selectedIndex == 1 || selectedIndex == 2) {
+            } else if (selectedIndex == 1) {
               TutorialMessage("아닌데...다시 한번 잘 생각해봐!").tutorialMessage(context);
             } else {
               TutorialMessage("어떤 계획을 세워야할 지 선택해줘!").tutorialMessage(context);
