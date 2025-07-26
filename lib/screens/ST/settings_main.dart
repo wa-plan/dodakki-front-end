3a93dfe20176eb6e715f5d4a5779240cc3e33877
bomul0304 <bomulphs@naver.com>
2025-07-26 14:49:22 +0900

    baseURL 수정

--- lib/screens/ST/settings_main.dart
+++ lib/screens/ST/settings_main.dart
@@ -330,7 +330,7 @@ class _SettingsMainState extends State<SettingsMain> {
                 )),
             SizedBox(width: 15),
             if (menu == '버전')
-              Text('1.0.14',
+              Text('1.0.15',
                   style: TextStyle(
                     fontSize: 15,
                     color: Colors.white,
